1477e1898dcadffce5be3ccaae64e4c8db4d1146
Nikita Parmon <nikitka.parmon@mail.ru>
2021-11-23 20:16:04 +0300

    Source classes

---
+++ Files/Files/Classes2.cs
@@ -0,0 +1,28 @@
+﻿namespace SrcFiles
+{
+    public class Class4
+    {
+        public void Method1()
+        {
+
+        }
+
+        public void Method2()
+        {
+
+        }
+    }
+
+    public class Class5
+    {
+        public void Method1()
+        {
+
+        }
+
+        private void Method2()
+        {
+
+        }
+    }
+}
