33572e215d3f17ece0168772ba13935ab8a2998c
Nikita Parmon <nikitka.parmon@mail.ru>
2021-11-23 20:14:19 +0300

    Application impl

---
+++ App/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Business;
+
+namespace App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var generatingOptions = new GeneratingOptions();
+            Console.WriteLine("Enter source path");
+            generatingOptions.SourceDirectory = @"D:\study\myspp\4\SrcFiles\Files";//Console.ReadLine();
+            Console.WriteLine("Enter destination path");
+            generatingOptions.DestinationDirectory = @"D:\study\myspp\4\Tests";//Console.ReadLine();
+            Generator.GenerateAsync(generatingOptions).Wait();
+        
+        }
+    }
+}
