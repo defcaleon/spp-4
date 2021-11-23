eaa44b69c5f7196d5d8ba74a7a84bb9f24c07fe1
Nikita Parmon <nikitka.parmon@mail.ru>
2021-11-23 20:15:34 +0300

    test generator logic implementation

---
+++ Logic/GeneratingOptions.cs
@@ -0,0 +1,16 @@
+﻿namespace Business
+{
+    public class GeneratingOptions
+    {
+        public int MaxRead { get; set; } = 5;
+
+        public int MaxGenerate { get; set; } = 5;
+
+        public int MaxWrite { get; set; } = 5;
+
+        public string SourceDirectory { get; set; }
+
+        public string DestinationDirectory { get; set; }
+    }
+
+}
---
+++ Logic/WriterOptions.cs
@@ -0,0 +1,16 @@
+﻿namespace Business
+{
+
+    public class WriterOptions
+    {
+        public string Filename { get; }
+
+        public string Content { get; }
+
+        public WriterOptions(string filename, string content)
+        {
+            Filename = filename;
+            Content = content;
+        }
+    }
+}
